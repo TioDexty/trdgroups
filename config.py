b53a320835a1d54468498ec931b72f759cd1229c
TioDexty <79750723+TioDexty@users.noreply.github.com>
2021-03-18 20:23:16 -0300

    Update and rename config.py.example to config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = "SEU:TOKEN"
+
+SUDOERS = [957539786, 957539786]
+TRD_CHAT = "@DextyOficial" do canal ou id
+API_ID = 1711524
+API_HASH = "c93b573669b12ffd1c099f62690a2ef6"
+
+VERSION = open("version.txt").read().rstrip()
